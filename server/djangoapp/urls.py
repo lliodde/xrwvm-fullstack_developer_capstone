642c15d152c3ecdacb4e9f166192736b92ee69b4
Dax <gapuzfernandofej@gmail.com>
2025-06-15 02:57:47 +0800

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
 
     # path for logout
-    path(route='logout', view=views.logout_user, name='logout'),# path for dealer reviews view
+    path(route='logout', view=views.logout_user, name='logout'),
 
     # path for get cars
     path(route='get_cars', view=views.get_cars, name='getcars'),
